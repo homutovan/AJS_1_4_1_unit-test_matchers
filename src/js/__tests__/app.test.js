fadac9ca766217bcce71996db0329b220bab1420
Andrey Khomutov <homutovan@live.com>
2020-08-13 21:38:15 +0700

    fix bugs

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,9 +1,13 @@
 import { characters, sortByHealth } from '../app';
 
+const sortCharacters = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { ame: 'мечник', health: 10 },
+];
+
 test('are not the exact same can', () => {
-  expect([...characters].sort(sortByHealth)).not.toBe(
-    [...characters].sort((a, b) => b.health - a.health),
-  );
+  expect([...characters].sort(sortByHealth)).not.toBe(sortCharacters);
 });
 
 test('sorting an array of characters', () => {
@@ -11,7 +15,5 @@ test('sorting an array of characters', () => {
 });
 
 test('have all the same properties', () => {
-  expect([...characters].sort(sortByHealth)).toEqual(
-    [...characters].sort((a, b) => b.health - a.health),
-  );
+  expect([...characters].sort(sortByHealth)).toEqual(sortCharacters);
 });
