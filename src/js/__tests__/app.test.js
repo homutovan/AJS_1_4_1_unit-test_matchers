cbb5c1fd504a973e848948ec81c2c119ccee1dc5
Andrey Khomutov <homutovan@live.com>
2020-08-19 01:20:10 +0700

    add some test case

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,19 +1,50 @@
-import { characters, sortByHealth } from '../app';
+import { sortByHealth } from '../app';
 
 const sortCharacters = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'лучник', health: 80 },
+  { ame: 'мечник', health: 10 },
+];
+
+const unsortCharacters = [
+  { name: 'лучник', health: 80 },
   { name: 'маг', health: 100 },
   { name: 'лучник', health: 80 },
   { ame: 'мечник', health: 10 },
 ];
 
-test('are not the exact same can', () => {
-  expect([...characters].sort(sortByHealth)).not.toBe(sortCharacters);
+const reverseSortCharacters = [
+  { ame: 'мечник', health: 10 },
+  { name: 'лучник', health: 80 },
+  { name: 'лучник', health: 80 },
+  { name: 'маг', health: 100 },
+];
+
+test('sorting unsort list not to be sort list', () => {
+  expect([...unsortCharacters].sort(sortByHealth)).not.toBe(sortCharacters);
 });
 
-test('sorting an array of characters', () => {
-  expect([...characters].sort(sortByHealth)).not.toEqual(characters);
+test('sorting sort list to equal sort list', () => {
+  expect([...sortCharacters].sort(sortByHealth)).toEqual(sortCharacters);
 });
 
-test('have all the same properties', () => {
-  expect([...characters].sort(sortByHealth)).toEqual(sortCharacters);
+test('sorting reverse sort list to equal sort list', () => {
+  expect([...reverseSortCharacters].sort(sortByHealth)).toEqual(sortCharacters);
+});
+
+test('sorting reverse sort list not to equal unsort list', () => {
+  expect([...reverseSortCharacters].sort(sortByHealth)).not.toEqual(unsortCharacters);
+});
+
+test('sorting unsort list not to equal reverse sort list', () => {
+  expect([...unsortCharacters].sort(sortByHealth)).not.toEqual(reverseSortCharacters);
+});
+
+test('sorting sort list not to equal reverse sort list', () => {
+  expect([...sortCharacters].sort(sortByHealth)).not.toEqual(reverseSortCharacters);
+});
+
+test('sorting unsort list to equal sort list', () => {
+  expect([...unsortCharacters].sort(sortByHealth)).toEqual(sortCharacters);
 });
